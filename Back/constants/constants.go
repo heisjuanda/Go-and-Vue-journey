99f9bd3306eaa6c7d30be1f468d58c166caf68ae
Juan David Moreno Alfonso <juandavid@cressco.dev>
2024-03-16 16:06:17 -0500

    add: important values for our functions

---
+++ Back/constants/constants.go
@@ -0,0 +1,14 @@
+package constants
+
+const UseName string = "admin"
+const Password string = "Complexpass#123"
+const IndexerName string = "email-indexer"
+
+const InfoBrowsers string = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"
+
+const MethodPost string = "POST"
+const Server string = "http://localhost:4080"
+const Endpoint string = "/api/email-indexer/_search"
+
+const MyServerPort string = "8080"
+const MyEndpoint string = "/search"
