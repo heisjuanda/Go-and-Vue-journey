8f6bf85e48c90dabd1a70777253f7c7c3350067d
Juan David Moreno Alfonso <juandavid@cressco.dev>
2024-04-08 22:41:12 -0500

    update: configuration for aws deployment

--- Back/main.go
+++ Back/main.go
@@ -22,3 +22,47 @@ func main() {
 	listenAndServeText := fmt.Sprintf(":%s", constants.SEARCH_SERVER_PORT)
 	http.ListenAndServe(listenAndServeText, handler)
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"io"
+// 	"log"
+// 	"net/http"
+// 	"strings"
+// )
+
+// func main() {
+// 	query := `{
+//         "search_type": "matchphrase",
+//         "query":
+//         {
+//             "term": "hi"
+//         },
+//         "from": 0,
+//         "max_results": 20,
+//         "_source": []
+//     }`
+// 	req, err := http.NewRequest("POST", "http://localhost:4080/api/email-indexer/_search", strings.NewReader(query))
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	req.SetBasicAuth("admin", "Complexpass#123")
+// 	req.Header.Set("Content-Type", "application/json")
+// 	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36")
+
+// 	client := &http.Client{}
+// 	resp, err := client.Do(req)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer resp.Body.Close()
+
+// 	log.Println(resp.StatusCode)
+// 	body, err := io.ReadAll(resp.Body)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	fmt.Println(string(body))
+// }
